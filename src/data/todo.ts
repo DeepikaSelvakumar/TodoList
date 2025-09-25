6fa0c7ba759b1e254787611c41c882a489b733dc
DeepikaSelvakumar <deepikaaselvakumar@gmail.com>
2025-09-25 21:58:31 +0530

    Initial commit

---
+++ src/data/todo.ts
@@ -0,0 +1,22 @@
+import type { Todo } from "../types/todo";
+
+export const dummyData : Todo []= [
+    {
+        id : 1,
+        title : "Like this video",
+        completed : false,
+
+    },
+    {
+        id : 2,
+        title : "Subscribe",
+        completed : false,
+
+    },
+    {
+        id : 3,
+        title : "Comment",
+        completed : false,
+
+    },
+]
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",   // For React/Vite projects
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
